136f1e03bcd8cf22723b63c7ebdba7597395d51d
RIckyfer02 <rickyferattack@gmail.com>
2025-10-31 13:33:10 +0100

    Compresse immagini pesanti pt. 2

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,9 @@
 import { React, useState } from "react";
 import Slider from "react-slick";
-import image1 from "../assets/images/Carousel/carousel1.JPG";
-import image2 from "../assets/images/Carousel/carousel2.JPG";
-import image3 from "../assets/images/Carousel/carousel3.JPG";
-import image4 from "../assets/images/Carousel/carousel4.JPG";
+import image1 from "../assets/images/Carousel/carousel1.jpg";
+import image2 from "../assets/images/Carousel/carousel2.jpg";
+import image3 from "../assets/images/Carousel/carousel3.jpg";
+import image4 from "../assets/images/Carousel/carousel4.jpg";
 import "../index.css";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
